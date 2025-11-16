614d5c3e4c09e5e7e5e48bf39f3a3573ac73a263
Alanribc <alan12alan34@gmail.com>
2025-11-16 00:08:23 -0300

    mudando do tiled map para mapa do proprio libdgx

---
+++ assets/mapas/labirintoteste.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wall1" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="sprites/wall1.jpg" width="225" height="225"/>
+</tileset>
